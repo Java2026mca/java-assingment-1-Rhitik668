7c44b7971ccd65a2b69f94d05f349f026f463c9a
Rhitik668 <rhitikdixit668@gmail.com>
2026-04-07 20:56:22 +0530

    solve q3

--- src/q03/Main.java
+++ src/q03/Main.java
@@ -3,18 +3,69 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
 
-        // TODO: Fill an N×N matrix in clockwise spiral order starting from 1
-        //       Print each row with values separated by single space
-        //       Then print: "Diagonal: X" where X = sum of primary diagonal (top-left to bottom-right)
-        //
-        // Input: 3
-        // Output:
-        // 1 2 3
-        // 8 9 4
-        // 7 6 5
-        // Diagonal: 15
+        // Input rows and columns
+        int r = sc.nextInt();
+        int c = sc.nextInt();
 
+        int[][] matrix = new int[r][c];
+
+        // Input matrix
+        for (int i = 0; i < r; i++) {
+            for (int j = 0; j < c; j++) {
+                matrix[i][j] = sc.nextInt();
+            }
+        }
+
+        // 🔹 Spiral Traversal
+        int top = 0, bottom = r - 1;
+        int left = 0, right = c - 1;
+
+        while (top <= bottom && left <= right) {
+
+            for (int i = left; i <= right; i++) {
+                System.out.print(matrix[top][i] + " ");
+            }
+            top++;
+
+            for (int i = top; i <= bottom; i++) {
+                System.out.print(matrix[i][right] + " ");
+            }
+            right--;
+
+            if (top <= bottom) {
+                for (int i = right; i >= left; i--) {
+                    System.out.print(matrix[bottom][i] + " ");
+                }
+                bottom--;
+            }
+
+            if (left <= right) {
+                for (int i = bottom; i >= top; i--) {
+                    System.out.print(matrix[i][left] + " ");
+                }
+                left++;
+            }
+        }
+
+        // New line after spiral output
+        System.out.println();
+
+        // 🔹 Diagonal Sum (only if square matrix)
+        int sum = 0;
+
+        if (r == c) {
+            for (int i = 0; i < r; i++) {
+                sum += matrix[i][i];
+
+                if (i != r - i - 1) {
+                    sum += matrix[i][r - i - 1];
+                }
+            }
+        }
+
+        System.out.println(sum);
+
+        sc.close();
     }
 }
