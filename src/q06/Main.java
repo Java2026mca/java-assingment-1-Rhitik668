e7d66e13a8a29a136fd311a0f1391e363d314555
Rhitik668 <rhitikdixit668@gmail.com>
2026-04-07 21:32:52 +0530

    solve q6

--- src/q06/Main.java
+++ src/q06/Main.java
@@ -1,15 +1,25 @@
-import java.util.*;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
 
-        // TODO: Print first n Fibonacci numbers separated by spaces
-        //       F(0)=0, F(1)=1, F(n)=F(n-1)+F(n-2)
-        //
-        // Input: 8
-        // Output: 0 1 1 2 3 5 8 13
+        int n;
+        System.out.print("Enter number of terms: ");
+        n = sc.nextInt();
 
+        int a = 0, b = 1;
+
+        System.out.println("Fibonacci Series:");
+
+        for (int i = 1; i <= n; i++) {
+            System.out.print(a + " ");
+
+            int next = a + b;
+            a = b;
+            b = next;
+        }
+
+        sc.close();
     }
 }
