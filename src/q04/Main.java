deded8ee5cfd84098abc22fb94199c36872efd22
Rhitik668 <rhitikdixit668@gmail.com>
2026-04-07 21:00:28 +0530

    solve q4

--- src/q04/Main.java
+++ src/q04/Main.java
@@ -1,25 +1,47 @@
-import java.util.*;
+import java.util.Scanner;
 
-public class Main {
+public class NumberClassifier {
+
+    // Method to check prime number
+    public static boolean isPrime(int num) {
+        if (num <= 1) return false;
+        for (int i = 2; i <= Math.sqrt(num); i++) {
+            if (num % i == 0)
+                return false;
+        }
+        return true;
+    }
+
+    // Method to check perfect number
+    public static boolean isPerfect(int num) {
+        if (num <= 1) return false;
+        int sum = 0;
+        for (int i = 1; i <= num / 2; i++) {
+            if (num % i == 0)
+                sum += i;
+        }
+        return sum == num;
+    }
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
 
-        // TODO: Read n integers. For each number print:
-        //   "Prime"     if it is prime
-        //   "Perfect"   if it is a perfect number (sum of proper divisors == itself, e.g. 6=1+2+3)
-        //   "Both"      if it is both (there are none < 100, but handle it)
-        //   "Neither"   otherwise
-        //
-        // Input:
-        // 4
-        // 6 13 8 28
-        //
-        // Output:
-        // Perfect
-        // Prime
-        // Neither
-        // Perfect
+        System.out.print("Enter a number: ");
+        int number = sc.nextInt();
 
+        boolean prime = isPrime(number);
+        boolean perfect = isPerfect(number);
+
+        if (prime && perfect) {
+            System.out.println(number + " is BOTH Prime and Perfect.");
+        } else if (prime) {
+            System.out.println(number + " is a Prime number.");
+        } else if (perfect) {
+            System.out.println(number + " is a Perfect number.");
+        } else {
+            System.out.println(number + " is neither Prime nor Perfect.");
+        }
+
+        sc.close();
     }
 }
