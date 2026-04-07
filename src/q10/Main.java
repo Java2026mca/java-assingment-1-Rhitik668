b76bca8b42920f779e6d6cdcc8d476c706a84887
Rhitik668 <rhitikdixit668@gmail.com>
2026-04-07 21:47:23 +0530

    solve q10

--- src/q10/Main.java
+++ src/q10/Main.java
@@ -1,21 +1,52 @@
-import java.util.*;
+import java.util.Scanner;
 
 public class Main {
+
+    static int evaluatePostfix(String exp) {
+        int[] stack = new int[100];
+        int top = -1;
+
+        for (int i = 0; i < exp.length(); i++) {
+            char ch = exp.charAt(i);
+
+            // Ignore spaces
+            if (ch == ' ') continue;
+
+            // If operand (digit)
+            if (Character.isDigit(ch)) {
+                stack[++top] = ch - '0';
+            }
+            // If operator
+            else {
+                int b = stack[top--];
+                int a = stack[top--];
+
+                int result = 0;
+
+                switch (ch) {
+                    case '+': result = a + b; break;
+                    case '-': result = a - b; break;
+                    case '*': result = a * b; break;
+                    case '/': result = a / b; break;
+                }
+
+                stack[++top] = result;
+            }
+        }
+
+        return stack[top];
+    }
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        // TODO: Evaluate a postfix (Reverse Polish Notation) expression
-        //       Operands are non-negative integers, operators are: + - * /
-        //       Use a stack to evaluate
-        //       Input: single line, tokens separated by spaces
-        //       Output: integer result
-        //
-        // Input: 5 1 2 + 4 * + 3 -
-        // Output: 14
-        //
-        // Explanation: 5 + ((1+2)*4) - 3 = 5 + 12 - 3 = 14
+        System.out.print("Enter postfix expression: ");
+        String exp = sc.nextLine();
 
-        String line = sc.nextLine();
+        int result = evaluatePostfix(exp);
 
+        System.out.println("Result = " + result);
+
+        sc.close();
     }
 }
