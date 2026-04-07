a55ff0178430eb6663d7b2409c016450468c370d
Rhitik668 <rhitikdixit668@gmail.com>
2026-04-07 21:29:26 +0530

    solve q5

--- src/q05/Main.java
+++ src/q05/Main.java
@@ -1,59 +1,16 @@
+package q05;
+
 import java.util.Scanner;
 
-public class ShapeArea {
+public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        int choice;
+        // Example logic (you can modify as per your question)
+        System.out.print("Enter a number: ");
+        int n = sc.nextInt();
 
-        do {
-            // Menu
-            System.out.println("\n=== Shape Area Calculator ===");
-            System.out.println("1. Area of Circle");
-            System.out.println("2. Area of Rectangle");
-            System.out.println("3. Area of Triangle");
-            System.out.println("4. Exit");
-            System.out.print("Enter your choice: ");
-            choice = sc.nextInt();
-
-            switch (choice) {
-                case 1:
-                    // Circle
-                    System.out.print("Enter radius: ");
-                    double r = sc.nextDouble();
-                    double circleArea = Math.PI * r * r;
-                    System.out.println("Area of Circle = " + circleArea);
-                    break;
-
-                case 2:
-                    // Rectangle
-                    System.out.print("Enter length: ");
-                    double length = sc.nextDouble();
-                    System.out.print("Enter breadth: ");
-                    double breadth = sc.nextDouble();
-                    double rectangleArea = length * breadth;
-                    System.out.println("Area of Rectangle = " + rectangleArea);
-                    break;
-
-                case 3:
-                    // Triangle
-                    System.out.print("Enter base: ");
-                    double base = sc.nextDouble();
-                    System.out.print("Enter height: ");
-                    double height = sc.nextDouble();
-                    double triangleArea = 0.5 * base * height;
-                    System.out.println("Area of Triangle = " + triangleArea);
-                    break;
-
-                case 4:
-                    System.out.println("Exiting program...");
-                    break;
-
-                default:
-                    System.out.println("Invalid choice! Please try again.");
-            }
-
-        } while (choice != 4);
+        System.out.println("You entered: " + n);
 
         sc.close();
     }
