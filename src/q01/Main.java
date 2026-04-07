d82f801c1d7d29dfd45f9999ae73a290170fe6b4
Rhitik668 <rhitikdixit668@gmail.com>
2026-04-07 20:49:15 +0530

    solve q1

--- src/q01/Main.java
+++ src/q01/Main.java
@@ -1,47 +1,46 @@
-import java.util.Scanner;
+import java.util.*;
 
-public class HollowDiamond {
+public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.print("Enter half height (n): ");
         int n = sc.nextInt();
 
         // Upper part
         for (int i = 1; i <= n; i++) {
-            // spaces
-            for (int j = i; j < n; j++) {
+
+            // spaces before
+            for (int j = 1; j <= n - i; j++) {
                 System.out.print(" ");
             }
 
-            // stars and hollow part
-            for (int j = 1; j <= (2 * i - 1); j++) {
-                if (j == 1 || j == (2 * i - 1)) {
+            // hollow stars
+            for (int j = 1; j <= 2 * i - 1; j++) {
+                if (j == 1 || j == 2 * i - 1) {
                     System.out.print("*");
                 } else {
                     System.out.print(" ");
                 }
             }
+
             System.out.println();
         }
 
         // Lower part
         for (int i = n - 1; i >= 1; i--) {
-            // spaces
-            for (int j = n; j > i; j--) {
+
+            for (int j = 1; j <= n - i; j++) {
                 System.out.print(" ");
             }
 
-            // stars and hollow part
-            for (int j = 1; j <= (2 * i - 1); j++) {
-                if (j == 1 || j == (2 * i - 1)) {
+            for (int j = 1; j <= 2 * i - 1; j++) {
+                if (j == 1 || j == 2 * i - 1) {
                     System.out.print("*");
                 } else {
                     System.out.print(" ");
                 }
             }
+
             System.out.println();
         }
-
-        sc.close();
     }
 }
